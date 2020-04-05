e044dbaa2d848866afb2cddb08793ef85b4c0c66
Tim <s110032@ap.be>
2020-04-05 16:37:19 +0200

    Git clean followup & drive basics update

--- Arduino/drive_basics/drive_basics.ino
+++ Arduino/drive_basics/drive_basics.ino
@@ -1,91 +1,98 @@
-#define MOTOR1_ENABLE 2
-#define MOTOR1_A 3
-#define MOTOR1_B 4
+// D2 pin naar 1,2EN van de SN754410, D3 pin naar 1A, D4 pin naar 2A
+#define M1_EN 2
+#define M1_A 3
+#define M1_B 4
 
-#define MOTOR2_ENABLE 5
-#define MOTOR2_A 6
-#define MOTOR2_B 7
+// D5 pin naar 3,4EN van de SN754410, D6 pin naar 3A, D7 pin naar 4A
+#define M2_EN 5
+#define M2_A 6
+#define M2_B 7
+
 
 void setup() {
-  pinMode(MOTOR1_ENABLE,OUTPUT);
-  pinMode(MOTOR1_A,OUTPUT);
-  pinMode(MOTOR1_B,OUTPUT);
-  digitalWrite(MOTOR1_ENABLE,HIGH);
-
-  pinMode(MOTOR2_ENABLE,OUTPUT);
-  pinMode(MOTOR2_A,OUTPUT);
-  pinMode(MOTOR2_B,OUTPUT);
-  digitalWrite(MOTOR2_ENABLE,HIGH);
+  pinMode(M1_EN, OUTPUT);
+  pinMode(M1_A, OUTPUT);
+  pinMode(M1_B, OUTPUT);
+  
+  pinMode(M2_EN, OUTPUT);
+  pinMode(M2_A, OUTPUT);
+  pinMode(M2_B, OUTPUT);
+  
+  digitalWrite(M1_EN, HIGH);
+  digitalWrite(M2_EN, HIGH);
 }
 
 void loop() {
-  // 5 sec forwards
+  // De waarden van deze parameters hebben een impact op hoe de tank rijd,
+  // bv bij een oppervlak met meer weerstand moet er langer gedraaid worden
+  
+  // 5 sec voorwaarts
   Forwards(5000);
-  //360 degree turn
+  // 360° draaien
   Left(5000);
-  // 5 sec backwards
+  // 5 sec achterwaarts
   Backwards(5000);
-  //180 degree turn
+  // 180° draaien
   Right(2500);
 
-  delay(1000);
+  delay(3000);
 }
 
 void Forwards(int duration)
 {
   //Left Track
-  digitalWrite(MOTOR1_A, HIGH); 
-  digitalWrite(MOTOR1_B, LOW);
+  digitalWrite(M1_A, HIGH); 
+  digitalWrite(M1_B, LOW);
   //Right Track
-  digitalWrite(MOTOR2_A, LOW);
-  digitalWrite(MOTOR2_B, HIGH);
+  digitalWrite(M2_A, LOW);
+  digitalWrite(M2_B, HIGH);
   delay(duration);
   Reset();
 }
 
 void Backwards(int duration)
 {
-  //Left Track
-  digitalWrite(MOTOR1_A, LOW);
-  digitalWrite(MOTOR1_B, HIGH);
-  //Right Track
-  digitalWrite(MOTOR2_A, HIGH); 
-  digitalWrite(MOTOR2_B, LOW);
+  // Left Track
+  digitalWrite(M1_A, LOW);
+  digitalWrite(M1_B, HIGH);
+  // Right Track
+  digitalWrite(M2_A, HIGH); 
+  digitalWrite(M2_B, LOW);
   delay(duration);
   Reset();
 }
 
 void Left(int duration)
 {
-  //Left Track
-  digitalWrite(MOTOR1_A, HIGH); 
-  digitalWrite(MOTOR1_B, LOW);
-  //Right Track
-  digitalWrite(MOTOR2_A, HIGH); 
-  digitalWrite(MOTOR2_B, LOW);
+  // Left Track
+  digitalWrite(M1_A, HIGH); 
+  digitalWrite(M1_B, LOW);
+  // Right Track
+  digitalWrite(M2_A, HIGH); 
+  digitalWrite(M2_B, LOW);
   delay(duration);
   Reset();
 }
 
 void Right(int duration)
 {
-  //Left Track
-  digitalWrite(MOTOR1_A, LOW);
-  digitalWrite(MOTOR1_B, HIGH);
-  //Right Track
-  digitalWrite(MOTOR2_A, LOW);
-  digitalWrite(MOTOR2_B, HIGH);
+  // Left Track
+  digitalWrite(M1_A, LOW);
+  digitalWrite(M1_B, HIGH);
+  // Right Track
+  digitalWrite(M2_A, LOW);
+  digitalWrite(M2_B, HIGH);
   delay(duration);
   Reset();
 }
 
 void Reset()
 {
-  //Left Track
-  digitalWrite(MOTOR1_A, LOW);
-  digitalWrite(MOTOR1_B, LOW);
-  //Right Track
-  digitalWrite(MOTOR2_A, LOW);
-  digitalWrite(MOTOR2_B, LOW);
+  // Left Track
+  digitalWrite(M1_A, LOW);
+  digitalWrite(M1_B, LOW);
+  // Right Track
+  digitalWrite(M2_A, LOW);
+  digitalWrite(M2_B, LOW);
   delay(100);
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SmartSystems-SensorsTeam
+
+[Panopto Video](https://ap.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=693a8b7b-d21c-4ef9-a6ca-ab9400c99b8f)
