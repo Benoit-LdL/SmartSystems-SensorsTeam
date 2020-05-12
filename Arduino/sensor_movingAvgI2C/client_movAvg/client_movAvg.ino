3ee024ea046fb4afc8f8904bd9765683247f6675
Tim <s110032@ap.be>
2020-05-12 12:15:40 +0200

    folderstructure fix

--- Arduino/sensor_movingAvgI2C/client_movAvg/client_movAvg.ino
+++ Arduino/sensor_movingAvgI2C/client_movAvg/client_movAvg.ino
@@ -1,12 +1,24 @@
 // download de library van https://github.com/AP-Elektronica-ICT/NewPing
 // en verwijder andere NewPing libraries
-
+#include <Wire.h>
 #include <NewPing.h>
 #define SONAR_NUM     4
 #define MAX_DISTANCE 200
 #define PING_INTERVAL 60
 
 #define SAMPLE_LENGTH 10
+
+
+typedef struct SensorData {
+  long movingAverage;
+  long rawData1[4];
+  
+} SensorData_t;
+
+SensorData_t lastSensorData1;
+const uint8_t SENSOR_DATA_SZ = sizeof(SensorData_t);
+const uint8_t SENSOR_SLAVE_ADDRESS = 8;
+byte buffer[SENSOR_DATA_SZ];
 
 uint8_t pingResults[SONAR_NUM];
 uint8_t pingHistory[SONAR_NUM][SAMPLE_LENGTH];
@@ -20,10 +32,20 @@ NewPing sonar[SONAR_NUM] = {
   NewPing(6, 5, MAX_DISTANCE)
 };
 
+void requestEvent() {
+  if (handled) {
+    Wire.write(buffer, SENSOR_DATA_SZ);
+    handled = false;
+  }
+}
+
 void setup() {
   Serial.begin(9600);
+   digitalWrite(SDA, LOW);
+  digitalWrite(SCL, LOW);
+  Wire.begin(SENSOR_SLAVE_ADDRESS);
+  Wire.onRequest(requestEvent);
   
-  // Setup I2C
 }
 
 void loop() {
@@ -31,13 +53,21 @@ void loop() {
     // Do ping and put in history[0]
     pingResults[i] = sonar[i].ping_cm();
     pingHistory[i][0] = pingResults[i];
-
+  } // @Tim kan da zijn da gy een haakje waard vergete hier
     // Do movingAvg conversion
     movingAvgResult[0] = sonar[0].convert_movingAverage(pingHistory[0]);
     movingAvgResult[1] = sonar[1].convert_movingAverage(pingHistory[1]);
     movingAvgResult[2] = sonar[2].convert_movingAverage(pingHistory[2]);
     movingAvgResult[3] = sonar[3].convert_movingAverage(pingHistory[3]);
 
+    // data versturen 
+   lastSensorData1 = {movingAvgResult[0],movingAvgResult[1],movingAvgResult[2],movingAvgResult[3]}};
+   memcpy(&buffer, &lastSensorData1, SENSOR_DATA_SZ);
+   if (!handled)
+   {
+   Serial.println("Requested");
+    handled = true;
+   }
 
     // Kan het zijn dat bij de shift de pingHistory array langer wordt dan [10]?
     // De averages worden nog niet correct getoond (foute input of berekening)
